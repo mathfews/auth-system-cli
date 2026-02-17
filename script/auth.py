e5aecdf603939fc6e9b9185577b74c9c2df1dce9
matheus <matheusfcw1010@gmail.com>
2026-02-17 00:37:50 -0300

    organize files

--- script/auth.py
+++ script/auth.py
@@ -46,35 +46,3 @@ class Auth:
             print("* User successfully registered!")
         else:
             print("Usuário already exists!")
-auth = Auth()
-def clean_terminal():
-    os.system("cls" if os.name == "nt" else "clear")
-while True:
-    print("Menu \n🔐1) Login\n📝2) Register")
-    userInput = input("> ").lower().strip()
-    if userInput == "login" or userInput == "1":
-        clean_terminal()
-        while True:
-            email = input("> Enter your email(Enter 0 to return): ").strip()
-            if email == "0":
-                clean_terminal()
-                break
-            password = input("> Enter your password: ").strip()
-            result = auth.login(email,password)
-            auth.show_login_results(email, password)
-            if result == True:
-                exit()
-    elif userInput == "register" or userInput == "2":
-        while True:
-            clean_terminal()
-            email = input("> Enter your email(Enter 0 to return): ").strip()
-            if email == "0":
-                clean_terminal()
-                break
-            username = input("> Enter your username: ").strip()
-            password = input("> Enter your password: ").strip()
-            result = auth.register(email,username, password)
-            auth.show_register_results(email)
-            if result == True:
-                clean_terminal()
-                break
